16a4143900fe57904e5c687639e0133a5e1dd627
ShaoyiLu <111807151+ShaoyiLu@users.noreply.github.com>
2023-03-18 13:41:04 -0500

    Update Q_learning.py

--- Project10/Q_learning.py
+++ Project10/Q_learning.py
@@ -26,7 +26,6 @@ if __name__ == "__main__":
     env.seed(1)
     env.action_space.np_random.seed(1)
 
-    # You will need to update the Q_table in your iteration
     Q_table = defaultdict(default_Q_value)  # starts with a pessimistic estimate of zero reward for each state.
     episode_reward_record = deque(maxlen=100)
 
@@ -35,10 +34,7 @@ if __name__ == "__main__":
         done = False
         obs = env.reset()
 
-        ##########################################################
-        # YOU DO NOT NEED TO CHANGE ANYTHING ABOVE THIS LINE
-        # TODO: Replace the following with Q-Learning
-
+        #Q-Learning
         while (not done):
             if random.uniform(0, 1) > EPSILON:
                 exception = np.array([Q_table[(obs, i)] for i in range(env.action_space.n)])
@@ -60,10 +56,6 @@ if __name__ == "__main__":
 
         EPSILON = EPSILON * EPSILON_DECAY
 
-        # END of TODO
-        # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE
-        ##########################################################
-
         # record the reward for this episode
         episode_reward_record.append(episode_reward)
 
@@ -71,8 +63,6 @@ if __name__ == "__main__":
             print("LAST 100 EPISODE AVERAGE REWARD: " + str(sum(list(episode_reward_record)) / 100))
             print("EPSILON: " + str(EPSILON))
 
-    #### DO NOT MODIFY ######
     model_file = open('Q_TABLE.pkl', 'wb')
     pickle.dump([Q_table, EPSILON], model_file)
     model_file.close()
-    #########################
